7d71fdceea173bebbe8ffb62a6aa71d905ca76c1
MiskyMikuy <miskymikuy.argentina@gmail.com>
2026-06-14 15:58:00 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  theme: { extend: {} },
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        misky: {
+          purple: '#602a80', /* El morado del logo */
+          red: '#d12229',    /* El rojo del logo */
+          green: '#127b46',  /* El verde de los adornos */
+          yellow: '#f4a62a', /* El amarillo/naranja */
+          bg: '#000000',     /* Fondo principal */
+          text: '#F3F0DF'    /* Texto claro */
+        }
+      }
+    },
+  },
   plugins: [],
-};
+}
